dc195a8ccb47146d2803e6e6b32222ee5bbb1a10
NefisaB <nefisa.hc@gmail.com>
2022-11-28 15:53:41 +0100

    Add tie logic and restart button

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const buttons = document.querySelectorAll(".game-field");
 const headingWinner = document.querySelector(".heading-winner");
 const namesDiv = document.querySelector(".player-names");
 const namesBtn = document.querySelector(".names-btn");
+const restartBtn = document.querySelector(".restart-btn");
+
+let count = 0;
 
 const displayController = (function () {
     const isGameOver = () => {
@@ -29,46 +32,64 @@ const displayController = (function () {
         return false;
     }
 
-    const endGame = () => {
-        const name = player1.active ? player1.name : player2.name;
-        headingWinner.textContent = `${name} has won!`;
+    const endGame = (isTie) => {
+        if (!isTie) {
+            const name = player1.active ? player1.name : player2.name;
+            headingWinner.textContent = `${name} has won!`;
+        } else {
+            headingWinner.textContent = "It' a tie!";
+        }
+        count = 0;
         headingWinner.classList.remove("hidden");
-        buttons.forEach(btn => btn.setAttribute("disabled", "true"));
+        buttons.forEach(btn => btn.disabled = true);
     }
 
-    return { isGameOver, endGame };
+    const restartGame = () => {
+        buttons.forEach(function (btn) {
+            btn.disabled = false;
+            btn.textContent = "";
+        });
+        namesDiv.classList.remove("hidden");
+        headingWinner.classList.add("hidden");
+        gameboard = [];
+        restartBtn.classList.add("hidden");
+    }
+
+    return { isGameOver, endGame, restartGame };
 })();
 
 
-const gameboard = [];
+let gameboard = [];
 
 const PlayerFactory = (name, letter, active) => {
-    const setName = (newName) => {
-        if (newName !== "") {
-            this.name = newName;
-        }
-    }
-    return { name, letter, active, setName };
+    return { name, letter, active };
 }
 
 const player1 = PlayerFactory("X", "X", true);
 const player2 = PlayerFactory("0", "0");
 
+
 function setButton() {
+    count++;
+    setNames();
+    namesDiv.classList.add("hidden");
+    restartBtn.classList.remove("hidden");
     const id = this.getAttribute("id");
     if (player1.active) {
         gameboard[id] = player1.letter;
         this.textContent = player1.letter;
         if (displayController.isGameOver()) {
-            displayController.endGame();
+            displayController.endGame(false);
+        } else if (count === 9) {
+            displayController.endGame(true);
         }
         player1.active = false;
 
     } else {
         gameboard[id] = player2.letter;
         this.textContent = player2.letter;
-        if (displayController.isGameOver()) {
-            displayController.endGame();
+        if (count === 9) {
+            displayController.endGame(true);
         }
         player1.active = true;
     }
@@ -78,17 +99,29 @@ function setButton() {
 
 }
 
-function addNames() {
-    player1.setName = document.querySelector("#player1").value;
+function setNames() {
     document.querySelector(".x-player").firstChild.textContent = player1.name;
     document.querySelector("#player1").value = "";
-    player2.setName = document.querySelector("#player2").value;
     document.querySelector(".o-player").firstChild.textContent = player2.name;
     document.querySelector("#player2").value = "";
+}
+
+function addNames() {
+    const nameOne = document.querySelector("#player1").value;
+    if (nameOne !== "") player1.name = nameOne;
+    console.log(player1.name);
+    console.log(player1.name);
+    const nameTwo = document.querySelector("#player2").value;
+    if (nameTwo !== "") player2.name = nameTwo;
+    setNames();
     namesDiv.classList.add("hidden");
+    console.log(player2.name);
+    restartBtn.classList.remove("hidden");
 }
 
 namesBtn.addEventListener("click", addNames);
 
 buttons.forEach(btn => btn.addEventListener("click", setButton));
+
+restartBtn.addEventListener("click", displayController.restartGame);
 
